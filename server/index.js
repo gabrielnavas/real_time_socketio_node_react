afcf24ca42060a931ba6217509c34e0f142fe5a2
Navas <gabrielnavas@email.com>
2021-02-15 17:29:15 -0300

    connection Chat component, using socket, with backend

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const socketio = require('socket.io')
-const http = require('http')
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+const router = require('./route');
 
-const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, { cors: { origin: "*", } });
 
-const router = require('./route')
-
-const app = express()
-
-const server = http.createServer(app)
-const io = socketio(server)
+app.use(cors());
+app.use(router);
 
 io.on('connection', socket => {
   console.log('We have a new connection')
@@ -19,6 +19,5 @@ io.on('connection', socket => {
   })
 })
 
-app.use(router)
 
-server.listen(PORT, () => console.log(`server has started on port ${PORT}`))
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
